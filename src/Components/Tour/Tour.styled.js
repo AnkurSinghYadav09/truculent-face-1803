6e0208585600792f56d0a309b6bbb1b822e71e75
AtulKumar69 <99542817+AtulKumar69@users.noreply.github.com>
2022-07-21 01:42:33 +0530

    Tour

---
+++ src/Components/Tour/Tour.styled.js
@@ -0,0 +1,184 @@
+import styled from "styled-components"
+export const Green=styled.div`
+width: 391px;
+    height: 402px;
+background:url(https://blog-cdn.everhour.com/assets/images/new-design/background-elements/dotted/oval-dotted-green.png);
+background-size: cover;
+position: absolute;
+    z-index: -1;
+    max-width: 100%;
+    margin-left:1000px;
+
+`
+export const C=styled.div`
+height:4000px;
+width:97%;
+border:1px solid orange;
+margin-left:20px;
+
+`
+export const Head=styled.div`
+width:90%;
+margin-top:100px;
+margin-left:140px;
+// border:1px solid black;
+display:flex;
+gap:10px;
+`
+export const Head1=styled.div`
+// border:1px solid red;
+width:100%;
+`
+export const P1=styled.div`
+font-size:20px;
+text-align:left;
+// line-height:7px;
+color:gray;
+`
+export const Head2=styled.div`
+// border:1px solid red;
+height:500px;
+width:80%;
+margin-right:10px;
+img{
+    height:415px;
+    width:661px;
+}
+`
+export const H1=styled.div`
+// border:1px solid green;
+width:80%;
+gap:-10px;
+margin-top:50px;
+p{
+    font-size:45px;
+    text-align:left;
+    gap:-10px;
+    margin-top:2px;
+    font-weight:bold;
+    // font-family: 'geomanist-book',sans-serif;
+    
+}
+`
+export const Inp=styled.div`
+width:80%;
+margin-top:30px;
+display:flex;
+height:50px;
+gap:10px;
+margin-left:-5px;
+// border:1px solid red;
+
+
+`
+export const I=styled.div`
+height:45px;
+width:60%;
+// border:1px solid blue;
+input{
+    height:87%;
+    width:90%;
+    border-radius:3px;
+    border-color:grey;
+    // padding-left:2px;
+    font-size:17px;
+
+}
+`
+export const Bt=styled.div`
+height:46px;
+width:35%;
+margin-left:-5px;
+
+// border:1px solid blue;
+button{
+    height:100%;
+    width:80%;
+    border:0;
+    border-radius:3px;
+    color:white;
+    background-color:#57bb71 ;
+}
+button:hover{
+        background-color:#3fcc8d ;
+}
+`
+export const G=styled.div`
+display:flex;
+gap:10px;
+margin-top:10px;
+padding-left:2px;
+// border:1px solid blue;
+a{
+    font-size:18px;
+}
+`
+export const Icon=styled.div`
+margin-top:2px;
+// border:1px solid red;
+font-size:21px;
+`
+//////////
+export const Time=styled.div`
+margin-top:100px;
+// border:1px solid black;
+img{
+    vertical-align: baseline;
+    height:673px;
+    width:1026px;
+    
+}
+
+`
+export const Tp=styled.p`
+margin-top:10px;
+margin-bottom:30px;
+`
+export const Blog=styled.div`
+display:grid;
+grid-template-columns:repeat(3,1fr);
+// border:1px solid black;
+height:700px;
+width:90%;
+margin-left:50px;
+margin-top:50px;
+gap:20px;
+`
+export const Logo=styled.div`
+height:20px;
+width:20px;
+display:flex;
+gap:10px;
+`
+export const Bld=styled.div`
+// border:1px solid green;
+height:150px;
+h3{
+    margin-top:-10px;
+    text-align:left;
+    margin-left:15px;
+}
+p{
+    margin-top:10px;
+    text-align:left;
+    margin-left:5px;
+    color:gray;
+}
+hr{
+    margin-top:50px;
+    width:90%;
+    // margin-left:20px;
+    background-color:#dcf1e1;
+}
+`
+////////////////////////////////////////////////////////////////
+export const Bill=styled.div`
+margin-top:100px;
+border:1px solid black;
+img{
+    vertical-align: baseline;
+    height:673px;
+    width:1026px;
+}
+
+`
