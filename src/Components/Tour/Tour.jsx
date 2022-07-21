d89b84edd7cdbdcac99c770ec658668c2862f073
AtulKumar69 <99542817+AtulKumar69@users.noreply.github.com>
2022-07-21 14:07:42 +0530

    tour head

--- src/Components/Tour/Tour.jsx
+++ src/Components/Tour/Tour.jsx
@@ -11,12 +11,27 @@ import { Bt } from "./Tour.styled";
 import { G } from "./Tour.styled";
 import { Icon } from "./Tour.styled";
 import { C } from "./Tour.styled";
+////////
 import { Time } from "./Tour.styled";
+import { Timehead } from "./Tour.styled";
+import { TimeH1 } from "./Tour.styled";
 import { Tp } from "./Tour.styled";
 import { Blog } from "./Tour.styled";
 import { Bld } from "./Tour.styled";
+/////////
 import { Bill } from "./Tour.styled";
+import { Billhead } from "./Tour.styled";
 import { Logo } from "./Tour.styled";
+import { Logoicon } from "./Tour.styled";
+/////////////////////////////////////////
+// import {Manage } from "./Tour.styled";
+
+
+
+
+
+
+
 
 const Tour = () => {
   return (
@@ -35,7 +50,6 @@ const Tour = () => {
             </P1>
             <Inp>
               <I>
-                {" "}
                 <input type="text" placeholder="Work email..." />
               </I>
               <Bt>
@@ -64,174 +78,179 @@ const Tour = () => {
           </Head2>
         </Head>
         <Time>
-          <div>
-            <h1>Time management</h1>
+          <Timehead>
+            <TimeH1>Time management</TimeH1>
             <Tp>
               Track time spent on projects, hours of work, vacations or sick
               days
             </Tp>
             <div>
-              <img
+              <img className="img-responsive"
                 src="https://blog-cdn.everhour.com/assets/images/new-design/screens/new-tour/time-tracking-timesheet-view.webp"
                 alt=""
               />
             </div>
-          </div>
+          </Timehead>
           <Blog>
             <Bld>
-              <div>
-                {/* <i class="icon-img icon-features icon-integration"></i> */}
-                <h3>Seemless integration</h3>
-              </div>
-              <div>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Seemless integrations</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Track time right inside the most popular apps, such as Asana,
-                  Basecamp, Jira, Trello, GitHub, ClickUp and more.
+                Track time right inside the most popular apps, such as Asana, Basecamp, Jira, Trello, GitHub, ClickUp and more.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
+                <hr />
+              </div>
+              </Bld>
+            <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Timer or manual entry</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
+                <p>
+                Start and stop your timer to record activities as you work. Or log hours manually afterwards.
+                </p>
+              </div>
+              <div style={{marginTop:"40px"}}>
+                <hr />
+              </div>
+              </Bld>
+            <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Clock-in, clock-out</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
+                <p>
+                Know the time spent on projects as well as hours of work and breaks.
+                </p>
+              </div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
             <Bld>
               <Logo>
-                <img src="/ghadi.png" alt="" />
-                <h3>Timer or manual entry</h3>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Estimates</h3></div> 
               </Logo>
-              <div>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Start and stop your timer to record activities as you work. Or
-                  log hours manually afterwards.
+                Set time estimates so at any time you can see the time you spend and what is left.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
             <Bld>
-              <div>
-                <h3>Clock-in, clock-out</h3>
-              </div>
-              <div>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Reminders</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Know the time spent on projects as well as hours of work and
-                  breaks.
+                Receive notifications about long-running timers and reminders to track time.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
             <Bld>
-              <div>
-                <h3>Estimates</h3>
-              </div>
-              <div>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Time off</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Set time estimates so at any time you can see the time you
-                  spend and what is left.
+                Track paid time off, vacations, sick days and leave types
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
             <Bld>
-              <div>
-                <h3>Reminders</h3>
-              </div>
-              <div>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Auto-stop timer</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Receive notifications about long-running timers and reminders
-                  to track time.
+                Configure to auto stop timer at the end of the workday.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
             <Bld>
-              <div>
-                <h3>Time off</h3>
-              </div>
-              <div>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Time log</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Track paid time off, vacations, sick days and leave types.
+                All changes and estimates made to the task time are logged and available for revision.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
             <Bld>
-              <div>
-                <h3>Auto-stop timer</h3>
-              </div>
-              <div>
-                <p>Configure to auto stop timer at the end of the workday.</p>
-              </div>
-              <div>
-                <hr />
-              </div>
-            </Bld>
-            <Bld>
-              <div>
-                <h3>Time log</h3>
-              </div>
-              <div>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Automate with Zapier</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  All changes and estimates made to the task time are logged and
-                  available for revision.
+                Use Zapier for automatic data transfer between Everhour and 1000+ apps with no technical knowledge.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
             <Bld>
-              <div>
-                <h3>Automate with Zapier</h3>
-              </div>
-              <div>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Browser extension</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Use Zapier for automatic data transfer between Everhour and
-                  1000+ apps with no technical knowledge.
+                Track time right from your browser’s toolbar with a single click.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
             <Bld>
-              <div>
-                <h3>Browser extension</h3>
-              </div>
-              <div>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>iPhone app</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Track time right from your browser’s toolbar with a single
-                  click.
+                Record time spent on projects and tasks from your iPhone.
                 </p>
               </div>
-              <div>
-                <hr />
-              </div>
-            </Bld>
-            <Bld>
-              <div>
-                <h3>iPhone app</h3>
-              </div>
-              <div>
-                <p>Record time spent on projects and tasks from your iPhone.</p>
-              </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
           </Blog>
         </Time>
         <Bill>
-          <div>
-            <h1>Billing and budgeting</h1>
+          <Billhead>
+            <TimeH1>Billing and budgeting</TimeH1>
             <Tp>
               Avoid debt and boost revenue with precise project billing and
               budgeting
@@ -242,134 +261,136 @@ const Tour = () => {
                 alt=""
               />
             </div>
-          </div>
+          </Billhead>
           <Blog>
-            <Bld>
-              <div>
-                <h3>Manage clients</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Manage clients</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Create clients, assign projects, watch budgets, spot
-                  uninvoiced time.
+                Create clients, assign projects, watch budgets, spot uninvoiced time.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
-            <Bld>
-              <div>
-                <h3>Flexible billing</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Flexible billing</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Choose an appropriate billing method: non-billable, time and
-                  materials or fixed fee.
+                Choose an appropriate billing method: non-billable, time and materials or fixed fee.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
-            <Bld>
-              <div>
-                <h3>Budgets</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Budgets</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Set up a capital or recurring budget for your projects. Don’t
-                  let finances catch you by surprise.
+                Set up a capital or recurring budget for your projects. Don’t let finances catch you by surprise.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
-            <Bld>
-              <div>
-                <h3>Alerts</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Alerts</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Receive an email alert when you reach a certain percentage of
-                  the project budget.
+                Receive an email alert when you reach a certain percentage of the project budget.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
-            <Bld>
-              <div>
-                <h3>Labor costs</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Labor costs</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Track what an employee or contractor costs you compared to how
-                  much you charge for their work.
+                Track what an employee or contractor costs you compared to how much you charge for their work.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
-            <Bld>
-              <div>
-                <h3>Non-billable time</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Non-billable time</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Exclude certain tasks from the billable amount calculation.
+                Exclude certain tasks from the billable amount calculation.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
-            <Bld>
-              <div>
-                <h3>Disallow overbudget</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Disallow overbudget</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Budget settings allow you to prohibit reporting time for
-                  anyone if the budget is exceeded.
+                Budget settings allow you to prohibit reporting time for anyone if the budget is exceeded.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
-            <Bld>
-              <div>
-                <h3>Custom task rates</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Custom task rates</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  You can override the base rate for a project and assign a
-                  specific rate to each task.
+                You can override the base rate for a project and assign a specific rate to each task.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
-            <Bld>
-              <div>
-                <h3>Budget visibility</h3>
-              </div>
-              <div>
+          <Bld>
+              <Logo>
+                <Logoicon><img src="public\logo\seemless.png" alt=""/></Logoicon>
+                <div style={{fontWeight:"bold"}}><h3>Budget visibility</h3></div> 
+              </Logo>
+              <div style={{marginTop:"20px"}}>
                 <p>
-                  Decide if members can see a project’s budget, which is visible
-                  to admins only by default.
+                Decide if members can see a project’s budget, which is visible to admins only by default.
                 </p>
               </div>
-              <div>
+              <div style={{marginTop:"40px"}}>
                 <hr />
               </div>
             </Bld>
           </Blog>
+          
         </Bill>
       </C>
     </>
--- src/Components/Tour/Tour.styled.js
+++ src/Components/Tour/Tour.styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 export const C=styled.div`
-height:4000px;
+height:5000px;
 width:97%;
 border:1px solid orange;
 margin-left:20px;
@@ -15,7 +15,7 @@ display:flex;
 gap:10px;
 `
 export const Head1=styled.div`
-// border:1px solid red;
+border:1px solid red;
 width:100%;
 `
 export const P1=styled.div`
@@ -25,10 +25,10 @@ text-align:left;
 color:gray;
 `
 export const Head2=styled.div`
-// border:1px solid red;
+border:1px solid red;
 height:500px;
-width:80%;
-margin-right:10px;
+width:100%;
+margin-right:5px;
 img{
     height:415px;
     width:661px;
@@ -64,13 +64,21 @@ export const I=styled.div`
 height:45px;
 width:60%;
 // border:1px solid blue;
+margin-left:5px;
+// border-color:gray;
 input{
-    height:87%;
-    width:90%;
-    border-radius:3px;
-    border-color:grey;
-    // padding-left:2px;
-    font-size:17px;
+    height:45px !important;
+    width:90% !important;
+    border-radius:3px !important;
+    border-color:gray !important;
+    outline-style: solid;
+    outline-color:gray;
+    outline-width: 1px !important;
+    padding-left:2px !important;
+    font-size:17px !important;
+    :hover{
+        outline-color:black !important;
+    }
 
 }
 `
@@ -110,64 +118,78 @@ font-size:21px;
 //////////
 export const Time=styled.div`
 margin-top:100px;
-// border:1px solid black;
-img{
-    vertical-align: baseline;
-    height:673px;
-    width:1026px;
-    
-}
+margin-bottom:100px;
+border:1px solid black;
+maring:auto;
 
+
+`
+export const Timehead=styled.div`
+width:75%;
+text-align:centre;
+margin:auto;
+justify-content:centre;
+border:1px solid blue;
+img{
+    width:100%;
+}
+`
+export const TimeH1=styled.h1`
+font-weight:bold;
+font-size:40px;
+text-align:center;
 `
 export const Tp=styled.p`
 margin-top:10px;
 margin-bottom:30px;
+text-align:center;
 `
 export const Blog=styled.div`
 display:grid;
 grid-template-columns:repeat(3,1fr);
-// border:1px solid black;
+border:1px solid black;
 height:700px;
-width:90%;
-margin-left:50px;
+width:85%;
+margin-left:100px;
 margin-top:50px;
-gap:20px;
+gap:30px;
 `
 export const Logo=styled.div`
-height:20px;
-width:20px;
 display:flex;
 gap:10px;
 `
+export const Logoicon=styled.div`
+height:20px;
+width:20px;
+`
 export const Bld=styled.div`
-// border:1px solid green;
-height:150px;
-h3{
-    margin-top:-10px;
-    text-align:left;
-    margin-left:15px;
-}
-p{
-    margin-top:10px;
-    text-align:left;
-    margin-left:5px;
-    color:gray;
-}
-hr{
-    margin-top:50px;
-    width:90%;
-    // margin-left:20px;
-    background-color:#dcf1e1;
-}
+margin-top:10px;
+line-height:20px;
+gap:10px;
 `
 ////////////////////////////////////////////////////////////////
 export const Bill=styled.div`
 margin-top:100px;
+margin-bottom:100px;
 border:1px solid black;
-img{
-    vertical-align: baseline;
-    height:673px;
-    width:1026px;
-}
+margin:auto;
 
 `
+export const Billhead=styled.div`
+width:75%;
+text-align:centre;
+margin:auto;
+justify-content:centre;
+border:1px solid blue;
+img{
+    width:100%;
+}`
+////////////////////////////////////////////////////////////////
+// export const Manage=styled.div`
+
+// margin-top:100px;
+// margin-bottom:100px;
+// border:1px solid black;
+// margin:auto;
+
+// `
