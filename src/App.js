2850a97d43ec73597482c134d2c5381b7f15f03f
paras0602 <parasjain912@gmail.com>
2022-07-23 09:48:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import Signup from "./Components/Signup/Signup";
-import { Landing } from "./Components/LandingPage/Landing";
+
 function App() {
   return (
     <div>   
-      {/* <Signup /> */}
-      <Landing/>
     </div>
   );
 }
