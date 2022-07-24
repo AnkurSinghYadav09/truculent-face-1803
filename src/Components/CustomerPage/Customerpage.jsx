bfc4558493ff3ecbc2eb4f914bdb0b0f3d54ed62
Papil Kamdi <papilkamdi786@gmail.com>
2022-07-24 10:43:08 +0530

    customer page finished

--- src/Components/CustomerPage/Customerpage.jsx
+++ src/Components/CustomerPage/Customerpage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Blog } from "../LandingPage/Blog/Blog";
+import  Boxes from "../LandingPage/Boxes/Boxes";
+import styles from "./CustomerPage.module.css";
 
 const Customerpage = () => {
   return (
-    <div>Customerpage</div>
-  )
-}
+    <div className={styles.container}>
+      <img
+        className={styles.center}
+        src="/logo/Customer-heading.png"
+        alt=""
+      ></img>
+      <br/>
+      <img className={styles.center} src="/logo/Customer1.png" alt=""></img>
+      <img
+        className={styles.center}
+        src="/logo/Inkedcustomer2.0.jpg"
+        alt=""
+      ></img>
+      <br/>
+      <br/>
+      <img
+        className={styles.center}
+        src="/logo/Inkedcustomer3.jpg"
+        alt=""
+      ></img>
+      <Boxes/>
+      <Blog/>
+    </div>
+  );
+};
 
-export default Customerpage
+export default Customerpage;
